e14fc25310d36953502a52ec88e4917c0eb1f252
nguyentrongtien <nguyenusb2005@gmail.com>
2020-10-21 22:26:54 +0700

    finish add fragment_above.xml and fragment_below.xml

--- app/src/main/java/com/example/tiki_layout/Fragment_Above.java
+++ app/src/main/java/com/example/tiki_layout/Fragment_Above.java
@@ -4,15 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FragmentBelow extends Fragment {
+public class Fragment_Above extends Fragment {
+
+    ImageButton imageBtnInc, imageBtnDec;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_below, container, false);
+
+        View viewAbove = inflater.inflate(R.layout.fragment_above, container, false);
+        //anh xa
+
+        return viewAbove;
     }
 }
--- app/src/main/java/com/example/tiki_layout/MainActivity.java
+++ app/src/main/java/com/example/tiki_layout/MainActivity.java
@@ -1,64 +1,18 @@
 package com.example.tiki_layout;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnTienhanhdathang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-        final FragmentAbove fragmentAbove = new FragmentAbove();
-        final FragmentBelow fragmentBelow = new FragmentBelow();
-
-        btnTienhanhdathang = findViewById(R.id.btnTienhanhdathang);
-
-//        getSupportFragmentManager()
-//                .beginTransaction()
-//                .add(R.id.frm_above, fragmentAbove, "fragmentAbove")
-//                .commit();
-
-        btnTienhanhdathang.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Fragment fragment
-                        = getSupportFragmentManager()
-                        .findFragmentByTag("fragmentAbove");
-
-                if(fragment!=null){
-                    Toast.makeText(MainActivity.this, "Exist!", Toast.LENGTH_SHORT).show();
-                }
-
-
-            }
-        });
-
-
-
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//        fragmentTransaction.add(R.id.frm_below,fragmentBelow);
-//        fragmentTransaction.commit();
-
-
-
-
-
-
-
 
     }
 }
